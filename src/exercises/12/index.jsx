7831b9e1265cb69f0d22571a84098e8e53566fb9
rkrux <5960750+rkrux@users.noreply.github.com>
2022-05-24 01:02:35 +0530

    tabs -> spaces

--- src/exercises/12/index.jsx
+++ src/exercises/12/index.jsx
@@ -1,78 +1,78 @@
 import { useState } from 'react';
 import {
-	atLeastOneItemOpenReducer,
-	atMostOneItemOpenReducer,
-	combineReducers,
-	Accordion,
-	AccordionItem,
-	AccordionPanel,
-	AccordionButtonOpen,
-	AccordionButtonClose,
+  atLeastOneItemOpenReducer,
+  atMostOneItemOpenReducer,
+  combineReducers,
+  Accordion,
+  AccordionItem,
+  AccordionPanel,
+  AccordionButtonOpen,
+  AccordionButtonClose,
 } from './accordion';
 
 const accordionData = Array.from({ length: 10 }, (_, index) => {
-	return {
-		id: index,
-		data: Math.floor(Math.random() * 1000),
-	};
+  return {
+    id: index,
+    data: Math.floor(Math.random() * 1000),
+  };
 });
 
 export default function Twelve() {
-	const [accordionConfig, setAccordionConfig] = useState({
-		atMostOneItemOpen: true,
-		atLeastOneItemOpen: true,
-	});
+  const [accordionConfig, setAccordionConfig] = useState({
+    atMostOneItemOpen: true,
+    atLeastOneItemOpen: true,
+  });
 
-	return (
-		<>
-			<div>
-				<ul>
-					<li>
-						atMostOneItemOpen: {String(accordionConfig.atMostOneItemOpen)}{' '}
-						{'->'}
-						<button
-							onClick={() => {
-								setAccordionConfig({
-									...accordionConfig,
-									atMostOneItemOpen: !accordionConfig.atMostOneItemOpen,
-								});
-							}}
-						>
-							(change)
-						</button>
-					</li>
-					<li>
-						atLeastOneItemOpen: {String(accordionConfig.atLeastOneItemOpen)}{' '}
-						{'->'}
-						<button
-							onClick={() => {
-								setAccordionConfig({
-									...accordionConfig,
-									atLeastOneItemOpen: !accordionConfig.atLeastOneItemOpen,
-								});
-							}}
-						>
-							(change)
-						</button>
-					</li>
-				</ul>
-			</div>
-			<Accordion
-				reducer={combineReducers([
-					atLeastOneItemOpenReducer,
-					atMostOneItemOpenReducer,
-				])}
-			>
-				{accordionData.map((value) => {
-					return (
-						<AccordionItem>
-							<AccordionButtonOpen>▼ Open: {value.id} ▼</AccordionButtonOpen>
-							<AccordionButtonClose>▲ Close: {value.id} ▲</AccordionButtonClose>
-							<AccordionPanel>{JSON.stringify(value)}</AccordionPanel>
-						</AccordionItem>
-					);
-				})}
-			</Accordion>
-		</>
-	);
+  return (
+    <>
+      <div>
+        <ul>
+          <li>
+            atMostOneItemOpen: {String(accordionConfig.atMostOneItemOpen)}{' '}
+            {'->'}
+            <button
+              onClick={() => {
+                setAccordionConfig({
+                  ...accordionConfig,
+                  atMostOneItemOpen: !accordionConfig.atMostOneItemOpen,
+                });
+              }}
+            >
+              (change)
+            </button>
+          </li>
+          <li>
+            atLeastOneItemOpen: {String(accordionConfig.atLeastOneItemOpen)}{' '}
+            {'->'}
+            <button
+              onClick={() => {
+                setAccordionConfig({
+                  ...accordionConfig,
+                  atLeastOneItemOpen: !accordionConfig.atLeastOneItemOpen,
+                });
+              }}
+            >
+              (change)
+            </button>
+          </li>
+        </ul>
+      </div>
+      <Accordion
+        reducer={combineReducers([
+          atLeastOneItemOpenReducer,
+          atMostOneItemOpenReducer,
+        ])}
+      >
+        {accordionData.map((value) => {
+          return (
+            <AccordionItem>
+              <AccordionButtonOpen>▼ Open: {value.id} ▼</AccordionButtonOpen>
+              <AccordionButtonClose>▲ Close: {value.id} ▲</AccordionButtonClose>
+              <AccordionPanel>{JSON.stringify(value)}</AccordionPanel>
+            </AccordionItem>
+          );
+        })}
+      </Accordion>
+    </>
+  );
 }
